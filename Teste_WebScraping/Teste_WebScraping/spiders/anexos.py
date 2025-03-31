d85ce47db4a6b1c87f133721382fdfd53c6ed8c6
Andrei <andrei.rezende@hotmail.com>
2025-03-30 21:10:49 -0300

    implementar captura de links de anexos

--- Teste_WebScraping/Teste_WebScraping/spiders/anexos.py
+++ Teste_WebScraping/Teste_WebScraping/spiders/anexos.py
@@ -1,10 +1,18 @@
 import scrapy
-
+import os
+import requests
 
 class AnexosSpider(scrapy.Spider):
-    name = "anexos"
+    name = "anexos_downloader"
     allowed_domains = ["www.gov.br"]
     start_urls = ["https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"]
 
     def parse(self, response):
-        pass
+        links_pdfs = []
+        lista_links = response.xpath("/html/body/div[2]/div[1]/main/div[2]/div/div/div/div/div[2]/div/ol/li")
+
+        for item in lista_links:
+            link = item.xpath(".//a/@href").extract_first()
+            links_pdfs.append(link)
+
+    
